368aafb978c76cc08eb5b664ccccb4cb2307b4b2
nukopy <nukopy@gmail.com>
2025-11-13 17:52:18 +0900

    ロガーの修正 - デバッグしやすいようにファイル名、行番号などを出力するようにした

--- water-controller-relay/src/logger.rs
+++ water-controller-relay/src/logger.rs
@@ -5,7 +5,11 @@ use tracing::Level;
 pub fn logger_init(level: Level) -> io::Result<()> {
     let subscriber = tracing_subscriber::fmt()
         .with_max_level(level)
-        .with_target(false)
+        .with_target(true)
+        .with_thread_ids(true)
+        .with_thread_names(true)
+        .with_file(true)
+        .with_line_number(true)
         .finish();
 
     tracing::subscriber::set_global_default(subscriber).map_err(io::Error::other)
